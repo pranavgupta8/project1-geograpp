61fcba450d0bcee7191e82683cd05c69b130d86c
Pranav Gupta <pranavgupta8@hotmail.com>
2016-11-15 04:48:04 +0530

    js code complete with errors. data-push issue. autocomplete still not done. container error with jsapi.

--- js/app.js
+++ js/app.js
@@ -1,27 +1,31 @@
 $(document).ready(function(){
 
+	google.charts.load('current', {'packages':['corechart']});
+	google.charts.setOnLoadCallback(drawChart);
+
 	$('form').submit(function(e){
 		e.preventDefault();
 
 		countryName = $('input[name*="country-query"]').val();
 
-		timePeriod = $('input[name*="period-query"]').val();
+		timePeriod = parseInt($('select[name*="period-query"]').val());
 
 		showResults();
 	});
 
 });
 
-var countryName, timePeriod, sum, data;
+var countryName, timePeriod, sum;
+var data = new Array();
 
 var getRequest = function(){
 
 	var currentYear = new Date().getFullYear();
 	var effectiveYear = new Array();
 	
-	for(int i=timePeriod; i>=0; i--){
+	for(i=timePeriod; i>=0; i--)
 	effectiveYear.push(currentYear - i);
-	}
+	
 
 	$.each(effectiveYear, function(i, val){
 		url = 'http://api.population.io:80/1.0/population/' + val + '/' + countryName;
@@ -38,17 +42,8 @@ var getRequest = function(){
 			console.log(sum);
 		});
 
-		data.push([val, sum]);
+		data.push('['+ val + ',' + sum + ']');
 	});
-};
-
-var showResults = function(){
-
-	getRequest();
-
-	$('#results').css('width', '90%', 'height', '50%');
-
-	drawChart();
 };
 
 var drawChart = function(){
@@ -63,7 +58,16 @@ var drawChart = function(){
 		legend: {position: 'bottom'}
 	};
 
-	var chart = new google.visualization.LineChart($('#results'));
+	var chart = new google.visualization.LineChart(document.getElementById('results'));
 
 	chart.draw(data, options);
-}
+};
+
+var showResults = function(){
+
+	getRequest();
+
+	$('#results').css('width', '90%', 'height', '50%', 'display', 'default');
+
+	drawChart();
+};
